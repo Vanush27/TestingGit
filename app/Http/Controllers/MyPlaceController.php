81c16e5dd74321a510e21d9e447f0c152e27f75c
Vanush27 <vanushgrigoryan90gmail.com>
2021-11-19 20:43:52 +0400

    Test

--- app/Http/Controllers/MyPlaceController.php
+++ app/Http/Controllers/MyPlaceController.php
@@ -4,12 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-
 class MyPlaceController extends Controller
 {
    public function index(){
        return "this is page ";
    }
-
-   
 }
